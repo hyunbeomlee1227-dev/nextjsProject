f1c8a4808f71fe5ed08a0d06724f46f96e57dae0
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-03-20 15:31:40 +0900

    dummyjson post & supabase add

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+export default function PostList() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/posts')
+      .then((res) => res.json())
+      .then((res) => setPosts(res.posts))
+  }, [])
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link href={`/posts/${post.id}`}>
+            {post.id} / {post.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
