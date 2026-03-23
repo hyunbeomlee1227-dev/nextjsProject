dd026cd1ded9b3cf1279d058d5887c56e6d5bbba
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-03-23 11:16:26 +0900

    supabase equal single

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,14 +2,20 @@
 
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import { supabase } from '../supabase/supabase.js'
 
 export default function PostList() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState<
+    Array<{ id: number; title: string; content: string }>
+  >([])
+
+  const fetchData = async () => {
+    const { data: posts, error } = await supabase.from('posts').select('*')
+    setPosts(posts ?? [])
+  }
 
   useEffect(() => {
-    fetch('https://dummyjson.com/posts')
-      .then((res) => res.json())
-      .then((res) => setPosts(res.posts))
+    fetchData()
   }, [])
 
   return (
