02a6a8fd65890ab1baf6003a2d52bf871f00c67b
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-03-24 14:16:12 +0900

    Add comment register & remove

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -24,6 +24,7 @@ export default function PostDetail() {
   const { id } = useParams()
   const [post, setPost] = useState<Post | null>(null)
   const [comments, setComment] = useState<Comment[]>([])
+  const [content, setContent] = useState<string>('')
 
   const fetchPost = async () => {
     const { data: post, error } = await supabase
@@ -58,6 +59,32 @@ export default function PostDetail() {
     }
   }
 
+  const handleOnSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const { data, error } = await supabase.from('comments').insert({
+      post_id: id as string,
+      content,
+    })
+
+    if (error) {
+      console.log(error)
+    } else {
+      alert('댓글 작성 성공!')
+      setContent('')
+      fetchComments()
+    }
+  }
+
+  const handleOnCommentDelete = async (id: number) => {
+    const { error } = await supabase.from('comments').delete().eq('id', id)
+    if (error) console.log(error)
+    else {
+      alert('댓글삭제성공')
+      fetchComments()
+    }
+  }
+
   if (!post) return <>Loading...</>
 
   return (
@@ -68,9 +95,27 @@ export default function PostDetail() {
       <div>댓글</div>
       <ul>
         {comments.map((comment) => (
-          <li key={comment.id}> - {comment.content}</li>
+          <li key={comment.id}>
+            {' '}
+            - {comment.content}{' '}
+            <button
+              onClick={() => handleOnCommentDelete(comment.id)}
+              className="border p-1"
+            >
+              X
+            </button>
+          </li>
         ))}
       </ul>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          name="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button>댓글 작성</button>
+      </form>
       <button
         className="p-2 rounded border-1 hover:bg-gray-200"
         onClick={() => handleOnDelete(post.id)}
